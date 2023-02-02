a91a67115aae67a26d3640d1ae6c3dbce0f90c40
YAMRAJINSANE <107044264+YAMRAJINSANE@users.noreply.github.com>
2023-02-02 09:09:08 +0530

    Update SelectedFeatured.jsx

--- src/pages/SelectedFeatured.jsx
+++ src/pages/SelectedFeatured.jsx
@@ -1,11 +1,18 @@
 import client from '../Sanity/sanity'
 import React, { useContext ,useState ,useEffect} from 'react'
 import { GlobalData } from './_app'
+import { useRouter } from 'next/router';
+
 import moment from 'moment'
 
 function SelectedFeatured() {
   const {FeaturedPost} = useContext(GlobalData)
+  const router = useRouter();
+console.log("rOUTER",router)
+  
+  const id = router.asPath.split('/')[2];
 
+  console.log("This is ",id)
   const [News, setNews] = useState([])
 console.log(FeaturedPost)
   useEffect(() => {
@@ -27,7 +34,7 @@ console.log(FeaturedPost)
     })
    }, [])
 
-   const filteredData = News.filter(item => item._id == FeaturedPost);
+   const filteredData = News.filter(item => item._id == id);
 
   
   return (
@@ -35,14 +42,15 @@ console.log(FeaturedPost)
   {filteredData.map((g)=>{
     return(
       <div className='flex w-full justify-between  px-10 mt-[100px] md:flex-row flex-col '>
-<div className='w-[300px] flex  flex-col  items-center   px-2 h-[500px] bg-green-600'>
+<div className='w-[300px] flex relative  flex-col  items-center   px-2 h-[500px] bg-green-600'>
 
 </div>
 <div className='flex  flex-col   items-center   px-2'>
 <div className='h-auto md:w-[500px] w-[350px] flex flex-col gap-2 px-2 py-4  bg-white shadow-md rounded-md '>
 <img src={g.imageUrl}/>
         <h1 className='Laato px-3 md:px-0 w-full'>{g.title}</h1>
-        <p className=" text-[15px] Laato left-3   text-gray-500 px-10 ">Updated on {moment(g.publishedAt).format('dddd, MMMM Do YYYY')}</p>
+        <p className=" text-[10px] lg:text-[14px]  w-full     font-semibold  text-gray-400   ">Updated on {moment(g.publishedAt).format('dddd, MMMM Do YYYY')}</p>
+
         <p>
           {g.body}
         </p>
